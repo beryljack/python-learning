85cef7c8d2c215ff757bf563f7d0ad28a0f5a186
beryljack <treasurejohnson25@gmail.com>
2026-07-28 13:24:28 -0700

    integersAndFloats.py file created

---
+++ integersAndFloats.py
@@ -0,0 +1,30 @@
+
+print (2 % 2)
+print (3 % 2)
+print (4 % 2)
+print (5 % 2)
+
+print (3 * (2 + 1))
+print (abs(-3))
+print (round(3.75))
+print (round(3.75, 1))
+
+num_1 = 3
+num_2 = 2
+print (num_1 != num_2)
+print (num_1 > num_2)
+print (num_1 < num_2)
+print (num_1 >= num_2)
+
+num_1 = '100'
+num_2 = '200'
+print (num_1 + num_2)
+
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+print (num_1 + num_2)
+
+
