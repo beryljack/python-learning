09b1275f099c38ba3d3b5c933cd1db3876f0f8f3
beryljack <treasurejohnson25@gmail.com>
2026-07-29 13:27:44 -0700

    listTuplesAndSets.py file created

---
+++ listTuplesAndSets.py
@@ -0,0 +1,23 @@
+courses =['History', 'Math', 'Physics', 'CompSci']
+print (len(courses))
+print (courses[3])
+print (courses[-1])
+print (courses[0:2])
+courses = ['History', 'Math', 'Physics', 'CompSci']
+courses_2 = ['Art', 'Education',]
+courses.extend(courses_2)
+print (courses)
+courses =['History', 'Math', 'Physics', 'CompSci']
+courses.remove('Math')
+print (courses)
+courses =['History', 'Math', 'Physics', 'CompSci']
+num = [1, 5, 2, 4, 3]
+courses.sort(reverse=True)
+num.sort(reverse=True)
+print (courses)
+courses =['History', 'Math', 'Physics', 'CompSci']
+num [1, 5, 2, 4, 3]
+sortes_courses = sorted(courses)
+print (sorted_courses)
+print (max(nums))
+
